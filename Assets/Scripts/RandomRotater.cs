be30e0aba4486330c855f63d146550df90be79aa
JessinRa <jessin.bdg@gmail.com>
2019-03-09 11:36:38 +0700

    added asteroid, random rotater, destroy on hit

---
+++ Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour
+{
+    [SerializeField] private float tumble;
+
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
