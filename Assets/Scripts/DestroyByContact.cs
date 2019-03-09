d65a36b8fdc1df5595b80e4505045aa2d6e65733
JessinRa <jessin.bdg@gmail.com>
2019-03-09 11:49:48 +0700

    moving asteroid, on destroy explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyByContact : MonoBehaviour
-{
+public class DestroyByContact : MonoBehaviour {
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject playerExplosion;
+
     // Destroy everything that enters the trigger
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Boundary"){
+    void OnTriggerEnter(Collider other) {
+
+        if (other.tag == "Boundary") {
             return;
         }
-        
+
+        if (other.tag == "Player") {
+            createPlayerExplosion();
+        }
+
+        createExplosion();
         Destroy(other.gameObject);
         Destroy(this.gameObject);
+    }
+
+    private void createExplosion() {
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+
+    private void createPlayerExplosion() {
+        Instantiate(playerExplosion, transform.position, transform.rotation);
     }
 }
