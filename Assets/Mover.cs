4d779e8f1d5fbe04339294bf1b1f2157ee48e0ea
JessinRa <jessin.bdg@gmail.com>
2019-03-09 10:35:29 +0700

    Bolt prefab

---
+++ Assets/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    
+    [SerializeField] private float speed;
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.velocity = transform.forward * speed;
+    }
+}
