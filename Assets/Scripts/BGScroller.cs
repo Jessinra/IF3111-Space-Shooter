77e9cd2ef1a8ee9432c8a143939d9e4abbca58e4
JessinRa <jessin.bdg@gmail.com>
2019-03-09 17:52:43 +0700

    added bg scroller

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+	[SerializeField] private float scrollSpeed = -0.25F;
+	[SerializeField] private float tileSizeZ = 30.0F;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
